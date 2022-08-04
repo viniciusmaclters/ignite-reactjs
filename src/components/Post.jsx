473b70f7d745d1c05665b189e5b07d1d5bc770bd
Vinicius Maclters <vinicius.maclters@hotmail.com>
2022-08-04 09:34:09 -0300

    Componente Post

---
+++ src/components/Post.jsx
@@ -0,0 +1,33 @@
+import styles from './Post.module.css'
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img className={styles.avatar} src="https://github.com/viniciusmaclters.png" alt="" />
+          <div className={styles.authorInfo}>
+            <strong>Vinicius Maclters</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+
+        <time title='04 de Agosto às 07:43' dateTime='2022-08-04 07:43'>Publicado há 1h</time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+
+        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+
+        <p>👉 <a href="#">jane.design/doctorcare</a></p>
+
+        <p>
+          <a href="#">#novoprojeto</a>{' '}
+          <a href="#">#nlw</a>{' '}  
+          <a href="#">#rocketseat</a>
+        </p>
+      </div>
+    </article>
+  )
+}
